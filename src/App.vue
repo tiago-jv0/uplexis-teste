60693645b1212266f505f48bc9c2f33189d19895
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 19:22:13 -0300

    feat: add carousel and router-view in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,45 @@
 <template>
-  <app-card v-bind="card" />
+  <div class="page-container">
+    <carousel :per-page="1" :autoplay="true" :loop="true">
+      <slide v-for="banner in banners" :key="banner.id">
+        <banner v-bind="banner" />
+      </slide>
+    </carousel>
+    <router-view />
+  </div>
 </template>
 
 <script>
+import "vue-select/dist/vue-select.css";
+
+import { Carousel, Slide } from "vue-carousel";
+import Banner from "./components/Banner.vue";
+
+import BannersMock from "./mocks/banners.mock";
+
 export default {
   name: "App",
+  components: {
+    Banner,
+    Carousel,
+    Slide,
+  },
+  data() {
+    return {
+      banners: BannersMock,
+    };
+  },
 };
 </script>
 
+<style lang="scss" scoped>
+.page-container {
+  max-width: 198rem;
+  width: 100%;
+  margin: 0 auto;
+}
+</style>
+
 <style lang="scss">
-*,
-*::after,
-*::before {
-  margin: 0;
-  padding: 0;
-  box-sizing: inherit;
-}
-
-html {
-  //this defines what 1rem is
-  font-size: 62.5%; // 1 rem = 10px; 10px/16px = 62.5%
-  scroll-behavior: smooth;
-}
-
-body {
-  box-sizing: border-box;
-  padding: 3rem;
-}
-
-:root {
-  --white: #ffffff;
-  --primary: #f26f14;
-  --secondary: #ff8e49;
-  --grey-black: #444444;
-  --grey: #9e9e9e;
-}
+@import "./scss/global.scss";
 </style>
