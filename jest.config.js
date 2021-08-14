c7c588a6e2b4eafbeb21b3ae2b7984a42c3c7888
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 18:27:16 -0300

    feat: add new configurations to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  verbose: false,
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!**/node_modules/**",
+    "!src/**/*.mock.js",
+  ],
   preset: "@vue/cli-plugin-unit-jest",
 };
