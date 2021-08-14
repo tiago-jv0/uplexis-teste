649d4987dcbd25701da76739fb7a1096ee0e5169
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 18:37:45 -0300

    feat: add BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button :class="['button', `button--${variant}`]" v-on="$listeners">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: {
+    variant: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.button {
+  cursor: pointer;
+  border: none;
+  font-family: "Roboto", sans-serif;
+
+  background: transparent
+    linear-gradient(39deg, var(--secondary) 0%, var(--primary) 100%) 0% 0%
+    no-repeat padding-box;
+  border-radius: 0.5rem;
+  color: var(--white);
+  font-size: 1.8rem;
+
+  &--small {
+    padding: 0.6rem 1.8rem;
+  }
+
+  &--medium {
+    padding: 0.75rem 3rem;
+  }
+}
+</style>
---
+++ tests/unit/base-button.spec.js
@@ -0,0 +1,35 @@
+import { mount } from "@vue/test-utils";
+
+import BaseButton from "../../src/components/BaseButton";
+
+describe("BaseButton component", () => {
+  test("should render properly", () => {
+    const wrapper = mount(BaseButton, {
+      propsData: {
+        text: "Clique Aqui",
+        variant: "medium",
+      },
+    });
+
+    expect(wrapper.text()).toContain("Clique Aqui");
+  });
+
+  test("should emit onClick event when clicked", async () => {
+    const handleClick = jest.fn();
+    const wrapper = mount(BaseButton, {
+      propsData: {
+        text: "Clique Aqui",
+        variant: "medium",
+      },
+      listeners: {
+        click: handleClick,
+      },
+    });
+
+    await wrapper.find(".button").trigger("click");
+
+    await wrapper.vm.$nextTick(); // Wait until $emits have been handled
+
+    expect(handleClick).toBeCalled();
+  });
+});
