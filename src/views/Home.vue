6d170d756fb933a0596f7ea1506e4bb1e307344f
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 18:42:40 -0300

    feat: integrate Cards, Categories and V-Select to create Home component

---
+++ src/views/Home.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="page-container">
+    <div>
+      <categories
+        :selected-category="filterCardsBy"
+        @on-category-chosen="filterBy"
+        :items="categories"
+      />
+      <div class="container">
+        <span class="text">Ordernar:</span>
+        <div class="select">
+          <v-select
+            v-model="orderCardsBy"
+            label="name"
+            :options="orderOptions"
+            :reduce="(item) => item.orderBy"
+            :clearable="false"
+          ></v-select>
+        </div>
+      </div>
+      <cards :data="cards" :orderBy="orderCardsBy" :filter="filterCardsBy" />
+    </div>
+  </div>
+</template>
+
+<script>
+import "vue-select/dist/vue-select.css";
+
+import Cards from "../components/Cards.vue";
+import Categories from "../components/Categories.vue";
+
+import CategoriesMock from "../mocks/categories.mock";
+import CategoriesCardMock from "../mocks/categories-card.mock";
+
+export default {
+  name: "Home",
+  components: {
+    Categories,
+    Cards,
+  },
+
+  methods: {
+    filterBy(category) {
+      this.filterCardsBy = category;
+    },
+  },
+
+  data() {
+    return {
+      orderOptions: [
+        { name: "lancamento", orderBy: "releaseDate" },
+        { name: "preço", orderBy: "price" },
+      ],
+      orderCardsBy: "lancamento",
+      filterCardsBy: "Mundo",
+      categories: CategoriesMock,
+      cards: CategoriesCardMock,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.page-container {
+  padding: 1rem;
+}
+</style>
+
+<style lang="scss">
+.container {
+  margin: 2rem 0;
+  display: flex;
+  align-items: center;
+}
+.vs__selected {
+  font-size: 1.4rem;
+}
+.vs__dropdown-option {
+  font-size: 1.4rem;
+}
+
+.VueCarousel-dot {
+  display: none !important;
+}
+.text {
+  font-weight: bold;
+  font-size: 18px;
+}
+
+.select {
+  width: 25rem;
+  margin-left: 2rem;
+}
+</style>
