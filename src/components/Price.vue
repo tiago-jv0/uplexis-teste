4113c218c336df3095e4e362767256ce1ddb83aa
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 18:29:46 -0300

    feat: add price component

---
+++ src/components/Price.vue
@@ -0,0 +1,89 @@
+.
+<template>
+  <div :class="['price', colorModifier && `price--${colorModifier}`]">
+    <small
+      :class="[
+        'price__currency',
+        currencyModifier && `price__currency--${currencyModifier}`,
+      ]"
+      >R$</small
+    >
+    <span
+      :class="[
+        'price__amount',
+        amountModifier && `price__amount--${amountModifier}`,
+      ]"
+    >
+      {{ formattedPrice }}</span
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Price",
+  props: {
+    price: {
+      type: Number,
+    },
+    currencyModifier: {
+      type: String,
+      default: () => "",
+    },
+    colorModifier: {
+      type: String,
+      default: () => "white",
+    },
+    amountModifier: {
+      type: String,
+      default: () => "",
+    },
+  },
+
+  computed: {
+    formattedPrice() {
+      return this.price.toString().replace(".", ",");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.price {
+  &--white {
+    color: var(--white);
+  }
+
+  &--black {
+    color: var(--black);
+  }
+
+  &__currency {
+    font-size: 1.6rem;
+    color: var(--black);
+
+    &--large {
+      font-size: 2.2rem;
+      line-height: 4.5rem;
+    }
+
+    &--xlarge {
+      font-size: 3.6rem;
+    }
+  }
+
+  &__amount {
+    font-size: 1.6rem;
+
+    &--large {
+      font-size: 2.2rem;
+      line-height: 2.6rem;
+    }
+
+    &--xlarge {
+      font-size: 3.7rem;
+      line-height: 2.6rem;
+    }
+  }
+}
+</style>
---
+++ tests/unit/price.spec.js
@@ -0,0 +1,30 @@
+import { mount } from "@vue/test-utils";
+
+import Price from "../../src/components/Price.vue";
+
+describe("Price component", () => {
+  test("should render formatted price", () => {
+    const wrapper = mount(Price, {
+      propsData: {
+        price: 20.59,
+      },
+    });
+
+    expect(wrapper.vm.formattedPrice).toBe("20,59");
+  });
+
+  test("should render with modifiers", () => {
+    const wrapper = mount(Price, {
+      propsData: {
+        price: 20.59,
+        currencyModifier: "xlarge",
+        colorModifier: "black",
+        amountModifier: "small",
+      },
+    });
+
+    expect(wrapper.find(".price--black").exists()).toBeTruthy();
+    expect(wrapper.find(".price__currency--xlarge").exists()).toBeTruthy();
+    expect(wrapper.find(".price__amount--small").exists()).toBeTruthy();
+  });
+});
