e0daae08f4fff2f20793534c5792bfd15dbb1d01
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 19:21:13 -0300

    feat: add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,23 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import Home from "../views/Home.vue";
+
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+
+  {
+    path: "/details",
+    name: "details",
+    component: () => import("../views/Details.vue"),
+    props: true,
+  },
+];
 
 const router = new VueRouter({
   routes,
