b77b530673659254e3f8c3dcc44e34efc5304007
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 18:32:37 -0300

    feat: add card component tests

--- tests/unit/card.spec.js
+++ tests/unit/card.spec.js
@@ -2,23 +2,78 @@ import { mount } from "@vue/test-utils";
 
 import Card from "../../src/components/Card";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
+
 const mockCard = {
-  iconUrl: require("../../src/assets/university-solid.png"),
-  iconDescription: "Universidade",
-  title: "Reguladores",
+  icon: "university",
+  category: "Reguladores",
   description:
     "O aplicativo Balanço Patrimonial realiza a consulta de todos os balanços que são publicados nos Diários Oficiais de empresas S.A., de capital aberto e limitadas (LTDA) de grande porte.",
   price: 29.99,
 };
 
-test("should render properly", () => {
-  const wrapper = mount(Card, {
-    propsData: mockCard,
+describe("Card component", () => {
+  test("should render properly", () => {
+    const wrapper = mount(Card, {
+      propsData: mockCard,
+      stubs: {
+        FontAwesomeIcon,
+      },
+    });
+
+    expect(wrapper.text()).toContain("Reguladores");
+    expect(wrapper.find(".card__description").exists()).toBeTruthy();
+    expect(wrapper.text()).toContain(
+      "O aplicativo Balanço Patrimonial realiza a consulta de todos os balanços que são publicados nos Diários Oficiais de empresas S.A., de capital aberto e limitadas (LTDA) de grande porte."
+    );
   });
 
-  expect(wrapper.text()).toContain("Reguladores");
-  expect(wrapper.text()).toContain("R$ 29.99");
-  expect(wrapper.text()).toContain(
-    "O aplicativo Balanço Patrimonial realiza a consulta de todos os balanços que são publicados nos Diários Oficiais de empresas S.A., de capital aberto e limitadas (LTDA) de grande porte."
-  );
+  test("should should no description if none is passed", () => {
+    const wrapper = mount(Card, {
+      propsData: {
+        icon: "university",
+        category: "Reguladores",
+        price: 29.99,
+      },
+      stubs: {
+        FontAwesomeIcon,
+      },
+    });
+
+    expect(wrapper.find(".card__description").exists()).toBeFalsy();
+  });
+
+  test("should change route if cta button is clicked", async () => {
+    const push = jest.fn();
+    const $router = {
+      push,
+    };
+
+    const wrapper = mount(Card, {
+      propsData: mockCard,
+      stubs: {
+        FontAwesomeIcon,
+      },
+      mocks: {
+        $router,
+      },
+    });
+
+    await wrapper.find(".card__cta").trigger("click");
+
+    expect(push).toHaveBeenCalled();
+    expect(push).toHaveBeenCalledWith({
+      name: "details",
+      params: {
+        icon: "university",
+        category: "Reguladores",
+        description:
+          "O aplicativo Balanço Patrimonial realiza a consulta de todos os balanços que são publicados nos Diários Oficiais de empresas S.A., de capital aberto e limitadas (LTDA) de grande porte.",
+        price: 29.99,
+      },
+    });
+  });
 });
