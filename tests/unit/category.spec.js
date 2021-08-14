fa8b2c49947e4b5b05a34bfe1dff6948fbe01e37
Tiago Jose <tiago_jvo@outlook.com>
2021-08-14 18:33:54 -0300

    feat: add category component tests

--- tests/unit/category.spec.js
+++ tests/unit/category.spec.js
@@ -2,16 +2,55 @@ import { mount } from "@vue/test-utils";
 
 import Category from "../../src/components/Category";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
+
 const mockCategory = {
-  iconUrl: require("../../src/assets/university-solid.png"),
-  iconDescription: "Universidade",
+  icon: "university",
+  selected: false,
   category: "Reguladores",
 };
 
-test("should render properly", () => {
-  const wrapper = mount(Category, {
-    propsData: mockCategory,
+describe("Category Component", () => {
+  test("should render with selected false by default", () => {
+    const category = {
+      icon: "university",
+      category: "Reguladores",
+    };
+
+    const wrapper = mount(Category, {
+      propsData: category,
+      stubs: {
+        FontAwesomeIcon,
+      },
+    });
+
+    expect(wrapper.vm.$props.selected).toBeFalsy();
   });
 
-  expect(wrapper.text()).toContain("Reguladores");
+  test("should render properly", () => {
+    const wrapper = mount(Category, {
+      propsData: mockCategory,
+      stubs: {
+        FontAwesomeIcon,
+      },
+    });
+
+    expect(wrapper.find(".category--selected").exists()).toBeFalsy();
+
+    expect(wrapper.text()).toContain("Reguladores");
+  });
+
+  test("should render with the right modifier in when selected is passed class", () => {
+    const wrapper = mount(Category, {
+      propsData: { ...mockCategory, selected: true },
+      stubs: {
+        FontAwesomeIcon,
+      },
+    });
+
+    expect(wrapper.find(".category--selected").exists()).toBeTruthy();
+  });
 });
