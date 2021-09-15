de49c30973e38e83cf36aea0f10b68e6fdcc2b2a
Tiago Jose <tiago_jvo@outlook.com>
2021-09-15 16:13:46 -0300

    chore: add vuex and axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Vuex from "vuex";
 import router from "./router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -9,16 +10,22 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import vSelect from "vue-select";
 import VueCarousel from "vue-carousel";
 
+import rootStore from "./state";
+
 library.add(fas);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.component("v-select", vSelect);
 
 Vue.use(VueCarousel);
+Vue.use(Vuex);
 
 Vue.config.productionTip = false;
 
+const store = new Vuex.Store(rootStore);
+
 new Vue({
   router,
+  store,
   render: (h) => h(App),
 }).$mount("#app");
