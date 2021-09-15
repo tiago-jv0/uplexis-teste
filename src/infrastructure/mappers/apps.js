6ebf52d0603a2810d47d6b7c864f84c9bc89de94
Tiago Jose <tiago_jvo@outlook.com>
2021-09-15 16:14:59 -0300

    feat: add AppsMapper

---
+++ src/infrastructure/mappers/apps.js
@@ -0,0 +1,19 @@
+class AppsMapper {
+  static fromApiToApp(apps, icons) {
+    return apps.map((app, index) => {
+      const icon = icons[index].icon.replace("fas fa-", "");
+
+      return {
+        id: app.id,
+        name: app.name,
+        category: app.category,
+        price: app.price,
+        date: new Date(app.date).toLocaleDateString("pt-br"),
+        description: app.description,
+        icon,
+      };
+    });
+  }
+}
+
+export default AppsMapper;
